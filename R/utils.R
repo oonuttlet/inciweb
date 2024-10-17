8b4c023c0cb835a09fcb3ac9c00f09571de04fb4
oonuttlet <77589230+oonuttlet@users.noreply.github.com>
2024-10-17 17:22:13 -0400

    updated date handling

--- R/utils.R
+++ R/utils.R
@@ -1,20 +1,18 @@
-dms_to_dd <- function(deg, min, sec){
+dms_to_dd <- function(deg, min, sec) {
   deg_dd <- abs(as.numeric(deg))
-  min_dd <- as.numeric(min)/60
-  sec_dd <- as.numeric(sec)/3600
-  dd <- deg_dd+min_dd+sec_dd
+  min_dd <- as.numeric(min) / 60
+  sec_dd <- as.numeric(sec) / 3600
+  dd <- deg_dd + min_dd + sec_dd
 }
 
-clean_html_col <- function(x){
+clean_html_col <- function(x) {
   stringr::str_squish(gsub("<[^>]+>|\\n|&nbsp;", "", x))
 }
 
-plaintext_to_date <- function(x){
+plaintext_to_date <- function(x) {
   y <- gsub(" ago", "", x)
   p <- lubridate::period(y)
-  if(as.numeric(p) > 86400){
-    return(format(as.POSIXct(Sys.Date() - p), "%m-%d-%Y %H:%M:%S"))
-  }
-  z <- as.POSIXct(Sys.time() - p)
-  return(z)
+  ifelse(as.numeric(p) > 86400,
+         format(as.POSIXct(Sys.Date() - p), "%m-%d-%Y %H:%M:%S"),
+         format(as.POSIXct(Sys.time() - p), "%m-%d-%Y %H:%M:%S"))
 }
